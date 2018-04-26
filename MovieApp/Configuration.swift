f0f92bea28d45fc7ce601a753b04dc1fc8ac9dff
Douglas Queiroz <douglasqueiroz@Douglass-MacBook-Pro.local>
2018-04-26 19:28:02 -0300

    Create requester layer

--- MovieApp/Configuration.swift
+++ MovieApp/Configuration.swift
@@ -11,5 +11,4 @@ import Foundation
 class Configuration {
     static let URL_BASE = "https://api.themoviedb.org/3"
     static let KEY = "1f54bd990f1cdfb230adb312546d765d"
-    static let HEADER = ["X-CISIdentity": "BP0E2CAACB0E4DAA956206AC3B7396F3" as AnyObject]
 }
---
+++ MovieApp/Model/Gender/Gender.swift
@@ -0,0 +1,25 @@
+//
+//  Gender.swift
+//  MovieApp
+//
+//  Created by Douglas Queiroz on 26/04/18.
+//  Copyright © 2018 Douglas Queiroz. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class Gender: Mappable {
+    
+    var id: Int?
+    var name: String?
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        id <- map["id"]
+        name <- map["name"]
+    }
+}
