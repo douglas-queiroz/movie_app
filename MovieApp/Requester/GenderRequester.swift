e4546c75b28cc8e744e15824509dae998f506048
Douglas Queiroz <douglasqueiroz@Douglass-MacBook-Pro.local>
2018-04-26 19:43:13 -0300

    Create gender requester interface q

--- MovieApp/Requester/GenderRequester.swift
+++ MovieApp/Requester/GenderRequester.swift
@@ -11,11 +11,17 @@ import Alamofire
 import AlamofireObjectMapper
 import ObjectMapper
 
-class GenderRequester {
+protocol GenderRequesterInterface {
     
     typealias GendersResult = (_ genders:GendersResponse?, _ error:Error?) -> Void
     
-    func getAllGenders(completion:@escaping GendersResult) {
+    func getAllGenders(completion: GendersResult);
+    
+}
+
+class GenderRequester {
+    
+    func getAllGenders(completion:@escaping GenderRequesterInterface.GendersResult) {
         request(RouterAPI.genders).responseObject { (response: DataResponse<GendersResponse>) in
             switch response.result {
             case .success(let genders):
